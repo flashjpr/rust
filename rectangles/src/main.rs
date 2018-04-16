f17b17acad66a57cd9abe70e0685206e12086a84
flashjpr <flashjpr@gmail.com>
2018-04-16 23:09:16 +0100

    associate functions in rust -- great

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -32,19 +32,31 @@ fn main() {
         width: 32,
         height: 82,
     };
-    
+
     println!(
-        "The are of the rectangle is {} square pixels.", rect3.area()
+        "The are of the rectangle is {} square pixels.",
+        rect3.area()
     );
 
-
     // can a rect fit other rect?
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(32); // associated fcts are called using the :: syntax
+    println!("The area of rect3 (square of 32) is {}", rect4.area());
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -67,12 +79,26 @@ fn area3(rectangle: &Rectangle) -> u32 {
 }
 
 // define a method "on" the Rectangle struct
-impl Rectangle {    // implementation block
-    fn area(&self) -> u32 { // borrowed as taking ownership is not intended 
+impl Rectangle {
+    // implementation block
+    fn area(&self) -> u32 {
+        // borrowed as taking ownership is not intended
         self.width * self.height
     }
-    
+
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+}
+
+// associated functions (don't take self as a param)
+impl Rectangle { // multiple 'impl' blocks
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            // create a square
+            width: size,
+            height: size,
+        }
     }
 }
