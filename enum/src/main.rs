50be7b4c711314262056ad65137a12ea607351d8
flashjpr <flashjpr@gmail.com>
2018-04-21 03:40:52 +0100

    matching some, none; exhaustive matching and placeholders

--- enum/src/main.rs
+++ enum/src/main.rs
@@ -34,6 +34,21 @@ fn main() {
     println!("Litecoin is worth ${}.", value_in_dollars(Coin::Litecoin));
 
     println!("Value in cents for {}", value_in_cents(MetalCoin::Quarter(UsState::Alaska)));
+
+    // Matching with Optional<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("5 + 1 = {}", six.unwrap());
+    let none = plus_one(None);
+
+    // the '_' placeholder
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("One"),
+        2 => println!("Two"),
+        3 => println!("Three"),
+        _ => (), // '_' is wildcard, '()' is the unit value
+    }
 }
 
 enum IpAddrEx {
@@ -107,5 +122,12 @@ fn value_in_cents(coin: MetalCoin) -> u32 {
             println!("State quarter from {:?}!", state);
             25
         },
+    }
+}
+
+fn plus_one(x: Option<i8>) -> Option<i8> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
