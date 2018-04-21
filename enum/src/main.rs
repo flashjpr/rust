386633750925ba77bb9eabcfa0fe5fb5591bf85f
flashjpr <flashjpr@gmail.com>
2018-04-21 01:37:24 +0100

    Used std lib for playing with ip addresses

--- enum/src/main.rs
+++ enum/src/main.rs
@@ -1,15 +1,23 @@
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+
 fn main() {
     // create instances
-    let ipv4 = IpAddrKind::V4;
-    let ipv6 = IpAddrKind::V6;
+    let home = IpAddrEx::V4(127,0,0,1);
+    let loopback = IpAddrEx::V6(String::from("::1"));
 
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    // using the std library
+    let v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    let v6 = IpAddr::V6(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1));
+    assert_eq!("127.0.0.1".parse(), Ok(v4));
+    assert_eq!("::1".parse(), Ok(v6));
 
-    let loopback = IpAddr::V6(String::from("::1"));
+    assert_eq!(v4.is_ipv4(), true);
+    assert_eq!(v6.is_ipv6(), true);
 }
 
-enum IpAddr {
-    V4(String),
+enum IpAddrEx {
+    // attach data directly to each variant (no need for any extra structs)
+    V4(u8,u8,u8,u8),
     V6(String),
 }
 
