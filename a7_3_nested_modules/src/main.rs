e462bdb4d483221c5891a96040c85bea3c0f41c4
flashjpr <flashjpr@gmail.com>
2018-04-23 22:25:13 +0100

    refering to names in different modules

--- a7_3_nested_modules/src/main.rs
+++ a7_3_nested_modules/src/main.rs
@@ -9,10 +9,23 @@ pub mod a {
 // brings into scope only the 'of' module
 use a::series::of;
 
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+use TrafficLight::{Red, Yellow}; // or bring everything using '*' -- TrafficLight::*;
+
+
 fn main() {
     // the verbose way is the same as
     a::series::of::nested_modules();
 
     // -> this
     of::nested_modules();
+
+    let red = Red;
+    let yellow = Yellow;
+    let green = TrafficLight::Green;
 }
--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -6,5 +6,17 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+
+        // start from the root
+        ::client::connect();
+
+        // start from the parent of the current module
+        super::client::connect();
+
+        // both from above achieve the same, really
+
+        // or, make good 'use'
+        use super::client;
+        client::connect();
     }
 }
